bbf8ff04333cd70bfb4affaf49480d63495536ed
Andrew <andrewcarlisle90@gmail.com>
2025-09-25 22:53:16 +0100

    Update Outdoor Sprite System
    Update Outdoor Sprite System

--- constants/sprite_constants.asm
+++ constants/sprite_constants.asm
@@ -230,10 +230,10 @@ SPRITE_POKEMON EQU const_value
 	const SPRITE_PINSIR     ; e4
 	const SPRITE_SCYTHER    ; e5
 	const SPRITE_AEGISLASH  ; e6
-	const SPRITE_KOFFING    ; e7
-	const SPRITE_ARBOK      ; e8
-	const SPRITE_MAGMAR     ; e9
-	const SPRITE_ELECTABUZZ ; ea
+	const SPRITE_MEOWTH     ; e7
+	const SPRITE_MAREEP     ; e8
+	const SPRITE_RIOLU      ; e9
+	const SPRITE_MARILL     ; ea
 	const SPRITE_POLIWHIRL  ; eb
 	const SPRITE_NIDOKING   ; ec
 	const SPRITE_NIDOQUEEN  ; ed

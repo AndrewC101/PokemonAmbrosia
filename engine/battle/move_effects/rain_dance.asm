3fc8628117ac4abb032820d8acf533a0058f1061
Andrew <andrewcarlisle90@gmail.com>
2025-10-19 22:54:52 +0100

    Weather Moves Fail If Weather Already Up
    Weather Moves Fail If Weather Already Up

--- engine/battle/move_effects/rain_dance.asm
+++ engine/battle/move_effects/rain_dance.asm
@@ -1,10 +1,16 @@
 BattleCommand_StartRain:
-; startrain
+	ld a, [wBattleWeather]
+	cp WEATHER_RAIN
+	jr z, .failed
+
 	ld a, WEATHER_RAIN
 	ld [wBattleWeather], a
 	ld a, FIELD_EFFECT_DURATION
 	ld [wWeatherCount], a
-	farcall _CGB_BattleColors
 	call AnimateCurrentMove
 	ld hl, DownpourText
 	jp StdBattleTextbox
+
+.failed
+	call AnimateFailedMove
+	jp PrintButItFailed
--- engine/battle/move_effects/sunny_day.asm
+++ engine/battle/move_effects/sunny_day.asm
@@ -1,12 +1,17 @@
 BattleCommand_StartSun:
 ; startsun
+	ld a, [wBattleWeather]
+	cp WEATHER_SUN
+	jr z, .failed
+
 	ld a, WEATHER_SUN
 	ld [wBattleWeather], a
 	ld a, FIELD_EFFECT_DURATION
 	ld [wWeatherCount], a
-    ld a, 0
-	ld [wBattleTimeOfDay], a
-	farcall _CGB_BattleColors
 	call AnimateCurrentMove
 	ld hl, SunGotBrightText
 	jp StdBattleTextbox
+
+.failed
+	call AnimateFailedMove
+	jp PrintButItFailed

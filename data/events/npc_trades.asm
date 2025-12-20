3f484d32409fe50b8a0fcf29d2bdd8388c440c7a
Andrew <andrewcarlisle90@gmail.com>
2025-12-20 11:53:07 +0000

    Update Trade Mawile Details
    Update Trade Mawile Details

--- data/events/npc_trades.asm
+++ data/events/npc_trades.asm
@@ -8,7 +8,7 @@ ENDM
 NPCTrades:
 ; entries correspond to NPCTRADE_* constants
 	table_width NPCTRADE_STRUCT_LENGTH, NPCTrades
-	npctrade TRADE_DIALOGSET_COLLECTOR, TEDDIURSA,   MAWILE,       "Venus@@@@@@", $DD, $DD, METAL_COAT, 00666,    "Maxwell@@@@", TRADE_GENDER_EITHER
+	npctrade TRADE_DIALOGSET_COLLECTOR, TEDDIURSA,   MAWILE,       "Arelle@@@@@", $DD, $DD, METAL_COAT, 52042,    "Maxwell@@@@", TRADE_GENDER_EITHER
 	npctrade TRADE_DIALOGSET_COLLECTOR, LARVITAR,    CHARMANDER,   "Dante@@@@@@", $DD, $DD, CHARCOAL, 01314,      "Sparda@@@@@", TRADE_GENDER_EITHER
 	npctrade TRADE_DIALOGSET_HAPPY,     ALAKAZAM,    METAGROSS,    "ED209@@@@@@", $DD, $DD, METAL_COAT,   00209,  "OCP@@@@@@@@", TRADE_GENDER_EITHER
 	npctrade TRADE_DIALOGSET_GIRL,      KINGDRA,     SNORLAX,      "Hakuho@@@@@", $FF, $FF, LEFTOVERS,    01187,  "Miyagino@@@", TRADE_GENDER_EITHER

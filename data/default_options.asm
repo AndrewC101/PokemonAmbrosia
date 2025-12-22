edb4fe07268731e68bfdb3b35e3974391325a7b7
Andrew <andrewcarlisle90@gmail.com>
2025-12-22 10:05:12 +0000

    Fix Text Bug
    Fix Text Bug

--- data/default_options.asm
+++ data/default_options.asm
@@ -6,7 +6,7 @@ DefaultOptions:
 ; wTextboxFrame: frame 1
 	db FRAME_1
 ; wTextboxFlags: use text speed
-	db 1 << NO_TEXT_DELAY_F
+	db 1 << FAST_TEXT_DELAY_F
 ; wOptions2: menu clock on
 	db 1 << MENU_CLOCK
 	db 0 << FAST_BATTLES
